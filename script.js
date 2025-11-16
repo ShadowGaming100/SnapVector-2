da55515877c94d77636dc734ce8ddb0d736d794b
CubeZockii <169075971+CubeZockii@users.noreply.github.com>
2025-11-16 13:50:42 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
 
-    const API_BASE_URL = 'http://127.0.0.1:5000';
+    const API_BASE_URL = 'staff.rrhosting.eu:7658';
 
     const authView = document.getElementById('auth-view');
     const appView = document.getElementById('app-view');
