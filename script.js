9c09510feb75919d2b48877f536e19ab2bd9b80d
CubeZockii <169075971+CubeZockii@users.noreply.github.com>
2025-11-18 08:08:30 -0500

    Aktualisieren von script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
 
-    const API_BASE_URL = 'https://snapserve-server.sayrz.com';
+    const API_BASE_URL = 'https://snapserve-server.sayrz.com'; 
 
     const authView = document.getElementById('auth-view');
     const appView = document.getElementById('app-view');
