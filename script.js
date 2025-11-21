a3c0c3d48e046999f7d517bc4ea636f6ac84906e
CubeZockii <169075971+CubeZockii@users.noreply.github.com>
2025-11-21 01:29:29 -0500

    Aktualisieren von script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
 
-    const API_BASE_URL = 'https://snapserve-server.codelabworks.is-cool.dev';
+    const API_BASE_URL = 'https://snapvector-server.codelabworks.is-cool.dev';
 
     const authView = document.getElementById('auth-view');
     const appView = document.getElementById('app-view');
